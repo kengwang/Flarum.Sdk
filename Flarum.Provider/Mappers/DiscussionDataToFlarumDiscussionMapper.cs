55827c6ca579954e1f671acf3a29290ac92f02ec
Jeff Zeng <zhh13950510580@outlook.com>
2025-01-05 16:23:03 +0800

    [Feat]

--- Flarum.Provider/Mappers/DiscussionDataToFlarumDiscussionMapper.cs
+++ Flarum.Provider/Mappers/DiscussionDataToFlarumDiscussionMapper.cs
@@ -14,8 +14,43 @@ namespace Flarum.Provider.Mappers
         {
             return new FlarumDiscussion
             {
-
-            }
+                Title = data.Title,
+                Slug = data.Slug,
+                Subscription = data.Subscription,
+                CommentCount = data.CommentCount,
+                ParticipantCount = data.ParticipantCount,
+                ReplyTemplate = data.ReplyTemplate,
+                LastPostNumber = data.LastPostNumber,
+                Votes = data.Votes,
+                CreatedAt = data.CreatedAt,
+                LastPostedAt = data.LastPostedAt,
+                LastReadAt = data.LastReadAt,
+                BestAnswerSetAt = data.BestAnswerSetAt,
+                FrontDate = data.FrontDate,
+                CanReply = data.CanReply,
+                CanRename = data.CanRename,
+                CanDelete = data.CanDelete,
+                CanHide = data.CanHide,
+                IsApproved = data.IsApproved,
+                HasApproved = data.HasApproved,
+                HasUpvoted = data.HasUpvoted,
+                CanTag = data.CanTag,
+                CanEditRecipients = data.CanEditRecipients,
+                CanEditGroupRecipients = data.CanEditGroupRecipients,
+                CanEditUserRecipients = data.CanEditUserRecipients,
+                IsPrivateDiscusion = data.IsPrivateDiscusion,
+                CanSeeReactions = data.CanSeeReactions,
+                IsLocked = data.IsLocked,
+                CanLock = data.CanLock,
+                CanSplit = data.CanSplit,
+                CanMerge = data.CanMerge,
+                Front = data.Front,
+                CanSelectBestAnswer = data.CanSelectBestAnswer,
+                IsSticky = data.IsSticky,
+                CanViewWhoTypes = data.CanViewWhoTypes,
+                CanMarkUnread = data.CanMarkUnread,
+                CanManageReplyTemplates = data.CanManageReplyTemplates,
+            };
         }
     }
 }
